af7d3b0383802bad8713140309249198a48ed6c8
7sevenmobi <7sevenmobi@gmail.com>
2018-08-15 20:49:40 +0800

    modify dos.txt

--- node/server.js
+++ node/server.js
@@ -1,12 +1,11 @@
 
-// 创建一个server，浏览器访问http://localhost:3000
+// 创建一个server，浏览器访问http://localhost:8080
+const http = require('http');
 
 let info = '<h2>node haha</h2>';
-
-const http = require('http');
 
 const serv = http.createServer(function (req,res){
     res.writeHead(200,{'Content-Type':'text/html'});
     res.end(info);
-}).listen(3000);
+}).listen(8080);
 
