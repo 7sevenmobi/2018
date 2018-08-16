c597f6206f649c11bcecbb39d186a7e3e00452a2
7sevenmobi <7sevenmobi@gmail.com>
2018-08-17 01:02:47 +0800

    learn express

--- node/server.js
+++ node/server.js
@@ -10,7 +10,7 @@ const serv = http.createServer(function (req,res){
     // res.writeHead('301',{'location':'http://www.tmall.com'});
 
     res.writeHead(200,{'Content-Type':'text/html'});
-
+    
     res.end(info);
 }).listen(8080,'localhost');
 
