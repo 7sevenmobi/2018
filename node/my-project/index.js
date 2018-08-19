da28f01b0434246cbf7e121eefb3a386731a4595
7sevenmobi <7sevenmobi@gmail.com>
2018-08-20 00:56:23 +0800

    '8.20'

--- node/my-project/index.js
+++
@@ -1,6 +0,0 @@
-
-// 加载colors模块
-require('colors');
-
-console.log('hello,node'.rainbow);
-
--- node/myapp/routes/index.js
+++ node/myapp/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+let users = {
+  uid : 0,
+  uname : '7sevenmobi',
+  pwd : '123',
+  addr : 'wuhan'
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { user: users });
 });
 
 module.exports = router;
